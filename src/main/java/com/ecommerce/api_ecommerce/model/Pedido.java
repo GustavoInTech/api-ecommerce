b471e61965da0a3f69f8256743542650668b1877
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2025-11-16 17:40:05 -0300

    implementndo a entidade pedido

---
+++ src/main/java/com/ecommerce/api_ecommerce/model/Pedido.java
@@ -0,0 +1,54 @@
+package com.ecommerce.api_ecommerce.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime data;
+    private String status;// Ex: PEDENTE, PAGO
+
+    public Pedido() {
+    }
+
+    public Pedido(Long id, LocalDateTime data, String status) {
+        this.id = id;
+        this.data = data;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
